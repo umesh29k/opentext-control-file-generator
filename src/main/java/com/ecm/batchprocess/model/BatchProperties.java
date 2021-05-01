4456ec67e1d991e9aca43ffcd0a52b63c004498f
prabhat <prabhatkushbawh@gmail.com>
2021-05-01 17:39:50 +0530

    updates for invocation of OpCo

--- src/main/java/com/ecm/batchprocess/model/BatchProperties.java
+++ src/main/java/com/ecm/batchprocess/model/BatchProperties.java
@@ -3,59 +3,20 @@ package com.ecm.batchprocess.model;
 import java.util.List;
 
 public class BatchProperties {
-    private List<BatchProperties> opCos;
-    private String region;
-    private String siteIds;
-    private String siteNames;
-    public BatchProperties(String region, String siteIds, String siteNames, List<BatchProperties> opCos) {
-        this.region = region;
-        this.siteIds = siteIds;
-        this.siteNames = siteNames;
-    }
+    private List<OpCo> opCos;
 
-    // Without a default constructor, Jackson will throw an exception
-    public BatchProperties() {
-    }
-
-    public List<BatchProperties> getOpCos() {
+    public List<OpCo> getOpCos() {
         return opCos;
     }
 
-    public void setOpCos(List<BatchProperties> opCos) {
+    public void setOpCos(List<OpCo> opCos) {
         this.opCos = opCos;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getSiteIds() {
-        return siteIds;
-    }
-
-    public void setSiteIds(String siteIds) {
-        this.siteIds = siteIds;
-    }
-
-    public String getSiteNames() {
-        return siteNames;
-    }
-
-    public void setSiteNames(String siteNames) {
-        this.siteNames = siteNames;
     }
 
     @Override
     public String toString() {
         return "BatchProperties{" +
                 "opCos=" + opCos +
-                ", region='" + region + '\'' +
-                ", siteIds='" + siteIds + '\'' +
-                ", siteNames='" + siteNames + '\'' +
                 '}';
     }
 }
